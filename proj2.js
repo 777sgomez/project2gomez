4b460fbcd4701888824149a07643014c5efde5a9
Susan Gomez <susangomez@Susans-MacBook-Pro.local>
2023-07-10 11:05:44 -0400

    Added userChoiceW, gameTies, and computerChoiceL  and assigned all of them to specific id assigned from the html doc in proj2.js

--- proj2.js
+++ proj2.js
@@ -4,3 +4,7 @@ let tiesScore = 0;
 
 let computerChoice;
 let userChoice;
+
+const userChoiceW = document.getElementById("userChoiceW");
+const gameTies = document.getElementById("gameTies");
+const computerChoiceL = document.getElementById("computerChoiceL");
